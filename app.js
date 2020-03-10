cfd25b318d4ff71f9a8d756abb6504ef34082745
unknown <gustavodextrezubieta@gmail.com>
2020-03-10 01:14:58 -0500

    commit test5

--- app.js
+++ app.js
@@ -4,10 +4,51 @@ const { streamPostRoute } = require("./config");
 const streamRoute = require("./routes/stream");
 const videoUploadRoute = require("./routes/secret/videoUpload");
 const app = express();
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mysql = require('mysql');
 
 //ffmpeg -f dshow -rtbufsize 100M -i video="USB2.0 HD IR UVC WebCam":audio="Microphone (Realtek(R) Audio)" -f mpegts -codec:v mpeg1video -s 640x480 -b:v 800k -r 25 -bf 0 <outSource>
+
+var connection = mysql.createConnection({
+	host     : 'b0szyfe7nkk86y122jws-mysql.services.clever-cloud.com',
+	user     : 'ulbof20vmt20v0qi',
+	password : 'tpgziIpbxCZ0Y65kyvEH',
+	database : 'b0szyfe7nkk86y122jws'
+});
+
 app.use("/public", express.static(path.resolve(__dirname, './public')));
 app.use(streamPostRoute, videoUploadRoute);
-app.use("/stream", streamRoute);
+
+app.use("/", streamRoute);
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.post('/strm', function(request, response) {
+    var username = request.body.username;
+    var password = request.body.password;
+    if (username && password) {
+        connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+            if (results.length > 0) {
+                request.session.loggedin = true;
+                request.session.username = username;
+                response.redirect('/streaming');
+            } else {
+                response.send('Usuarios y/o contraseña incorrecta!');
+            }			
+            response.end();
+        });
+    } else {
+        response.send('Por favor ingrese un usuario y contraseña!');
+        response.end();
+    }
+});
 
 module.exports = app;
--- routes/stream.js
+++ routes/stream.js
@@ -3,7 +3,19 @@ const path = require("path");
 const router = Router();
 
 router.get('/', (req, res) => {
+
+    //Agregar lo comentado como principal cuando haya base de datos
+    const p = path.resolve(__dirname, '../', 'views', 'login.html');
+
+    //const p = path.resolve(__dirname, '../', 'views', 'stream.html');
+    res.sendFile(p);
+});
+
+
+router.get('/streaming', (req, res) => {
     const p = path.resolve(__dirname, '../', 'views', 'stream.html');
     res.sendFile(p);
 });
+
+
 module.exports = router;
